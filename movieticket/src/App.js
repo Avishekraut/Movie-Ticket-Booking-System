dfb7b7c8f7c156fba5012f85306654cdb3c99ec0
Avishek Raut <avishekraut123@gmail.com>
2023-12-06 15:09:07 +0545

    App.js Update

--- movieticket/src/App.js
+++ movieticket/src/App.js
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home';
-import BookingPage from './Pages/BookingPage';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import MovieDetails from "./Pages/MovieDetails";
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<BookingPage />} />
-      </Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+    </Routes>
   );
 }
 
