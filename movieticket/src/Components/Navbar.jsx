e477c7260a0a18186ab2ed96d73da635be31326e
Avishek Raut <avishekraut123@gmail.com>
2023-12-06 12:24:19 +0545

    Update Navbar component

--- movieticket/src/Components/Navbar.jsx
+++ movieticket/src/Components/Navbar.jsx
@@ -3,22 +3,22 @@ import { BiMoviePlay } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div className="flex mx-28 my-4 justify-between">
+    <div className="flex mx-32 my-4 justify-between">
       <div className="flex gap-24">
         <div className="flex items-center gap-1">
           <BiMoviePlay size={24} />
           <h1 className="text-xl font-medium">BookMyMovie</h1>
         </div>
-          <ul className="flex font-medium gap-8 items-center">
+          <ul className="flex font-medium gap-8 items-center cursor-pointer">
             <li>Home</li>
             <li>Movies </li>
             <li>New Releases</li>
             <li>Events</li>
           </ul>
       </div>
-      <div className="flex font-semibold items-center gap-4">
+      <div className="flex font-semibold items-center gap-4 cursor-pointer">
         <h2>Login</h2>
-        <button className="bg-primary hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
+        <button className="bg-primary hover:bg-[#cb3f4f] text-white font-semibold py-2 px-4 rounded">
           Register
         </button>
       </div>
