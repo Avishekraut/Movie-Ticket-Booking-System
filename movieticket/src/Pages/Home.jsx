eaa7c83a5a744e25eb777a91024612bb8e18bf87
Avishek Raut <avishekraut123@gmail.com>
2023-12-06 12:24:49 +0545

    Home page update

--- movieticket/src/Pages/Home.jsx
+++ movieticket/src/Pages/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Navbar from '../Components/Navbar';
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Card from "../Components/Card";
 
 const Home = () => {
+  //IMDb ID for the latest movies
+  const movieIds = ["tt13751694", "tt10786774", "tt29481471","tt18411490"];
+
   return (
     <div>
       <Navbar />
+      <div className="flex mx-32 my-20 gap-20">
+        {movieIds.map((id) => (
+          <Card key={id} movieId={id} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
